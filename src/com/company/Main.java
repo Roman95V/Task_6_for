5cfc85fb19abd6e7d2695b2fb3f64c88865755cd
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-11-27 17:50:58 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Input of odd numbers: ");
+        for(int i = 1; i < 99; i++) {
+            if( i % 2 !=0)
+                System.out.println(i);
+        }
+    }
+}
