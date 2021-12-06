8cabfa1509c43705a74ad7ad567ce320d7760893
voloshynromanmukol@gmail.com <voloshynromanmukol@gmail.com>
2021-12-06 20:45:57 +0200

    update

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Input of odd numbers: ");
         for(int i = 1; i < 99; i++) {
-            if( i % 2 !=0)
+            if( i % 2 != 0)
                 System.out.println(i);
         }
     }
